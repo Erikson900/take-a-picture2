df441f658f15b15a5817b802f6b943ba46c8be7f
Erik Ostrovski <Erik.ostrov@gmail.com>
2021-08-24 14:17:01 +0300

    Initial Commit

---
+++ take a picture2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  take a picture2
+//
+//  Created by Erik Ostrovski on 24/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
